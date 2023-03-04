a27238591c531658ba5daa0d83848c839cf3a775
Luiz Gonzaga <luizrodrigo46@hotmail.com>
2023-03-04 19:04:16 -0300

    Mais ajusters

--- produtos_e_servicos/produtos_e_servicos/settings.py
+++ produtos_e_servicos/produtos_e_servicos/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'produtos_e_servicos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['produtos_e_servicos'],
+        'DIRS': ['produtos_e_servicos', 'produtos'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- produtos_e_servicos/produtos_e_servicos/urls.py
+++ produtos_e_servicos/produtos_e_servicos/urls.py
@@ -23,5 +23,7 @@ from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('servicos', listagem_servicos, name='servicos'),
+    path('produtos', listagem_produtos, name='produtos'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
